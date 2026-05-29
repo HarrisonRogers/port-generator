c67da63d4ce87fa76e08603de78a33d26d664bc7
harrison rogers <harrison.rogers05@gmail.com>
2026-05-30 11:26:52 +1200

    add a heap of components and change the routes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,8 +5,6 @@ import {
 } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
 
 import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
 
@@ -51,10 +49,9 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <script dangerouslySetInnerHTML={{ __html: THEME_INIT_SCRIPT }} />
         <HeadContent />
       </head>
-      <body className="font-sans antialiased wrap-anywhere selection:bg-[rgba(79,184,178,0.24)]">
-        <Header />
+      <body className="font-sans antialiased wrap-anywhere ">
         {children}
-        <Footer />
+        {/* <Footer /> */}
         <TanStackDevtools
           config={{
             position: 'bottom-right',
