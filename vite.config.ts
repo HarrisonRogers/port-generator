d48e222fef9ab6c025261d7a9e4be3addb722ba2
harrison rogers <harrison.rogers05@gmail.com>
2026-06-01 13:21:13 +1200

    test new build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
-import { createRequire } from 'node:module'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
@@ -8,16 +7,11 @@ import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
 
-const require = createRequire(import.meta.url)
-const tslibPath = require.resolve('tslib/tslib.es6.js')
-
 const config = defineConfig({
-  resolve: {
-    alias: {
-      tslib: tslibPath,
-    },
-    tsconfigPaths: true,
+  ssr: {
+    noExternal: ['react-remove-scroll'],
   },
+
   plugins: [
     devtools(),
     nitro({ rollupConfig: { external: [/^@sentry\//] } }),
